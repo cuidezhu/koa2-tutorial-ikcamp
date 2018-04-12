a31a20528d3b0509052e9080b7fd91a5cd0c366e
cuidezhu <chinacuidezhu@gmail.com>
2018-04-12 17:10:54 +0800

    async await next

--- app.js
+++ app.js
@@ -2,18 +2,39 @@
 * @Author: cuidezhu
 * @Date:   2018-04-12 12:06:04
 * @Last Modified by:   cuidezhu
-* @Last Modified time: 2018-04-12 14:29:59
+* @Last Modified time: 2018-04-12 17:08:32
 */
 
 const Koa = require('koa')
 const app = new Koa()
 
 app.use(async (ctx, next) => {
+  let stime = new Date().getTime()
   await next()
+  let etime = new Date().getTime()
   ctx.response.type = 'text/html'
   ctx.response.body = '<h1>Hello World</h1>'
+  console.log(`请求地址: ${ctx.path}, 响应时间: ${etime - stime}ms`)
+});
+
+app.use(async (ctx, next) => {
+  console.log('中间件1 doSoming')
+  await next()
+  console.log('中间件1 end')
 })
 
-app.listen(3000, () => {
+app.use(async (ctx, next) => {
+  console.log('中间件2 doSoming')
+  // await next()
+  console.log('中间件2 end')
+})
+
+app.use(async (ctx, next) => {
+  console.log('中间件3 doSoming')
+  await next()
+  console.log('中间件3 end')
+})
+
+app.listen(3000, ()=> {
   console.log('server is running at http://localhost:3000')
 })
