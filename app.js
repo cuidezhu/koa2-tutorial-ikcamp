465933e140a3de7d6274bc2467324476d4367b8e
cuidezhu <chinacuidezhu@gmail.com>
2018-04-12 14:33:45 +0800

    require koa and hello world

--- app.js
+++ app.js
@@ -2,7 +2,18 @@
 * @Author: cuidezhu
 * @Date:   2018-04-12 12:06:04
 * @Last Modified by:   cuidezhu
-* @Last Modified time: 2018-04-12 12:06:33
+* @Last Modified time: 2018-04-12 14:29:59
 */
 
-console.log('Hello World')
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+  await next()
+  ctx.response.type = 'text/html'
+  ctx.response.body = '<h1>Hello World</h1>'
+})
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
+})
