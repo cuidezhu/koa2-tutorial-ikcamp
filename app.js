c545cc7e146ee57043043b5da614c8a608a6e6ae
cuidezhu <chinacuidezhu@gmail.com>
2018-04-13 16:02:26 +0800

    http get

--- app.js
+++ app.js
@@ -2,27 +2,32 @@
 * @Author: cuidezhu
 * @Date:   2018-04-12 12:06:04
 * @Last Modified by:   cuidezhu
-* @Last Modified time: 2018-04-13 03:13:17
+* @Last Modified time: 2018-04-13 16:00:48
 */
 
 const Koa = require('koa')
 const router = require('koa-router')()
 const app = new Koa()
 
-// add router
-router.get('/', async (ctx, next) => {
+router.get('/', async(ctx, next) => {
   ctx.response.body = '<h1>index page</h1>'
 })
 
-router.get('/home', async (ctx, next) => {
-  ctx.response.body = '<h1> HOME page</h1>'
+router.get('/home', async(ctx, next) => {
+  console.log(ctx.request.query)
+  console.log(ctx.request.querystring)
+  ctx.response.body = '<h1>HOME page</h1>'
 })
 
-router.get('/404', async (ctx, next) => {
+router.get('/home/:id/:name', async(ctx, next) => {
+  console.log(ctx.params)
+  ctx.response.body = '<h1>HOME page ' + ctx.params.id + ' ' + ctx.params.name + '</h1>'
+})
+
+router.get('/404', async(ctx, next) => {
   ctx.response.body = '<h1>404 Not Found</h1>'
 })
 
-// call router middleware
 app.use(router.routes())
 
 app.listen(3000, () => {
