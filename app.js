10ebd02c165dcdc7b4fac19d0b8ea6ee49c3997b
cuidezhu <chinacuidezhu@gmail.com>
2018-04-13 10:13:19 +0800

    koa router

--- app.js
+++ app.js
@@ -2,34 +2,28 @@
 * @Author: cuidezhu
 * @Date:   2018-04-12 12:06:04
 * @Last Modified by:   cuidezhu
-* @Last Modified time: 2018-04-13 02:46:39
+* @Last Modified time: 2018-04-13 03:13:17
 */
 
 const Koa = require('koa')
+const router = require('koa-router')()
 const app = new Koa()
 
-app.use(async (ctx, next) => {
-  if (ctx.request.path === '/') {
-    ctx.response.body = '<h1>index page</h1>'
-  } else {
-    await next()
-  }
-})
-app.use(async (ctx, next) => {
-  if (ctx.request.path === '/home') {
-    ctx.response.body = '<h1>home page</h1>'
-  } else {
-    await next()
-  }
+// add router
+router.get('/', async (ctx, next) => {
+  ctx.response.body = '<h1>index page</h1>'
 })
 
-app.use(async (ctx, next) => {
-  if (ctx.request.path === '/404') {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-  } else {
-    await next()
-  }
+router.get('/home', async (ctx, next) => {
+  ctx.response.body = '<h1> HOME page</h1>'
 })
+
+router.get('/404', async (ctx, next) => {
+  ctx.response.body = '<h1>404 Not Found</h1>'
+})
+
+// call router middleware
+app.use(router.routes())
 
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000')
