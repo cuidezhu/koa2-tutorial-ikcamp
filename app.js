e35f4e98f538b4f3420097b7a4a7507b27d7f334
cuidezhu <chinacuidezhu@gmail.com>
2018-04-13 18:51:44 +0800

    style

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 * @Author: cuidezhu
 * @Date:   2018-04-12 12:06:04
 * @Last Modified by:   cuidezhu
-* @Last Modified time: 2018-04-13 17:56:33
+* @Last Modified time: 2018-04-13 18:13:52
 */
 
 const Koa = require('koa')
@@ -11,8 +11,11 @@ const router = require('./router')
 const bodyParser = require('koa-bodyparser')
 const nunjucks = require('koa-nunjucks-2')
 const app = new Koa()
+const staticFiles = require('koa-static')
 
 app.use(bodyParser())
+
+app.use(staticFiles(path.resolve(__dirname, './public')))
 
 app.use(nunjucks({
   ext: 'html',
