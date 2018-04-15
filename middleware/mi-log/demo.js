fe73088a09da953ac0e45885c50afc175f3ef1b5
cuidezhu <chinacuidezhu@gmail.com>
2018-04-15 15:41:26 +0800

    require log4js

---
+++ middleware/mi-log/demo.js
@@ -0,0 +1,10 @@
+/*
+* @Author: cuidezhu
+* @Date:   2018-04-15 15:37:55
+* @Last Modified by:   cuidezhu
+* @Last Modified time: 2018-04-15 15:39:16
+*/
+var log4js = require('log4js')
+var logger = log4js.getLogger()
+logger.level = 'debug'
+logger.debug('Some debug messages')
