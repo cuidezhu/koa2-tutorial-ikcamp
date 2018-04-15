26ab05d27af6ebc7d68875bf74f7a43918c5e4ba
cuidezhu <chinacuidezhu@gmail.com>
2018-04-15 15:48:10 +0800

    log4js full example

--- middleware/mi-log/demo.js
+++ middleware/mi-log/demo.js
@@ -2,9 +2,18 @@
 * @Author: cuidezhu
 * @Date:   2018-04-15 15:37:55
 * @Last Modified by:   cuidezhu
-* @Last Modified time: 2018-04-15 15:39:16
+* @Last Modified time: 2018-04-15 15:45:02
 */
-var log4js = require('log4js')
-var logger = log4js.getLogger()
-logger.level = 'debug'
-logger.debug('Some debug messages')
+const log4js = require('log4js')
+log4js.configure({
+  appenders: { cheese: {type: 'file', filename: 'cheese.log'}},
+  categories: { default: { appenders: ['cheese'], level: 'error'}}
+})
+
+const logger = log4js.getLogger('cheese')
+logger.trace('Entering cheese testing')
+logger.debug('Got cheese.')
+logger.info('Cheese is Gouda.')
+logger.warn('Cheese is quite smelly.')
+logger.error('Cheese is too ripe!')
+logger.fatal('Cheese was breeding ground for listeria.')
